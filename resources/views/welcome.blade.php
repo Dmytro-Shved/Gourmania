85a4c20e28881c5b213e1bbe6ee58653f0de1f5d
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2024-12-30 17:36:40 +0100

    installed Flowbite package for svg icons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Title</title>
+    <link rel="icon" type="image/x-icon" href="https://i.ibb.co/2jPbxvC/logo-round.png">
+    <title>Gourmania | Recipes and more</title>
     @vite('resources/css/app.css')
 </head>
 <body>
+<div class="text-gray-700">
+    Hi gourmans
+</div>
 
+<script src="../path/to/flowbite/dist/flowbite.min.js"></script>
 </body>
 </html>
