fee5d3d5e572e0f1206660c77f28c5c148b9d6c9
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2025-01-10 09:54:19 +0100

    added carousele with images & title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -242,6 +242,7 @@
     <main class="flex-grow">
         <div class="font-inclusive text-xl">
 
+            {{-- Slogan --}}
             <div class="flex items-center justify-center my-4">
                 <span class="flex-grow border-t border-black"></span>
                 <small class="font-inknut text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-black px-4">
@@ -369,8 +370,101 @@
             </div>
 
             <br>
-            <hr class="border-t border-black">
-            <br>
+
+            {{-- Popular recipes --}}
+            <div class="flex items-center justify-center my-4">
+                <span class="flex-grow border-s border-8 border-[#AE763E] md:border-[10px] lg:border-[12px]"></span>
+                <small class="font-inknut text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-black px-4">
+                    POPULAR RECIPES
+                </small>
+                <span class="flex-grow border-s border-8 border-[#AE763E] md:border-[10px] lg:border-[12px]"></span>
+            </div>
+
+            {{-- Carousel --}}
+            <div x-data="{
+                        slides: [
+                            {
+                                {{-- Image 1 --}}
+                                imgSrc: 'https://images.wallpaperscraft.com/image/single/steak_mushrooms_asparagus_103375_1920x1080.jpg',
+                                imgAlt: 'A perfectly grilled steak served with potatoes and greens.',
+                                title: 'Delicious Steak',
+                                description: 'A tender and juicy steak, cooked to perfection, paired with crispy potatoes.',
+                                ctaUrl: 'https://example.com',
+                                ctaText: 'See more',
+                            },
+                            {
+                                {{-- Image 2 --}}
+                                imgSrc: 'https://images.wallpaperscraft.com/image/single/salmon_teriyaki_fish_108544_1920x1080.jpg',
+                                imgAlt: 'Fresh fish fillet served on a plate with soy sauce and sesame seeds.',
+                                title: 'Grilled Fish',
+                                description: 'A delicate and flavorful grilled fish, perfect for any seafood lover.',
+                                ctaUrl: 'https://example.com',
+                                ctaText: 'See more',
+                            },
+                            {
+                                {{-- Image 3--}}
+                                imgSrc: 'https://images.wallpaperscraft.com/image/single/pizza_pastry_appetizing_117398_1920x1080.jpg',
+                                imgAlt: 'Hot pizza with cheese and toppings, sliced and ready to eat.',
+                                title: 'Hot Pizza',
+                                description: 'A delicious pizza with crispy crust, melted cheese, and your favorite toppings.',
+                                ctaUrl: 'https://example.com',
+                                ctaText: 'See more',
+                            },
+                        ],
+                        currentSlideIndex: 1,
+                        previous() {
+                            if (this.currentSlideIndex > 1) {
+                                this.currentSlideIndex = this.currentSlideIndex - 1
+                            } else {
+                                this.currentSlideIndex = this.slides.length
+                            }
+                        },
+                        next() {
+                            if (this.currentSlideIndex < this.slides.length) {
+                                this.currentSlideIndex = this.currentSlideIndex + 1
+                            } else {
+                                this.currentSlideIndex = 1
+                            }
+                        },
+                    }" class="relative w-full overflow-hidden">
+
+                {{-- previous button --}}
+                <button type="button" class="absolute left-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center bg-white/40 p-2 text-neutral-600 transition hover:bg-white/60 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black active:outline-offset-0 dark:bg-neutral-950/40 dark:text-neutral-300 dark:hover:bg-neutral-950/60 dark:focus-visible:outline-white" aria-label="previous slide" x-on:click="previous()">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-width="3" class="size-5 md:size-6 pr-0.5" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                    </svg>
+                </button>
+
+                {{-- next button --}}
+                <button type="button" class="absolute right-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center bg-white/40 p-2 text-neutral-600 transition hover:bg-white/60 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black active:outline-offset-0 dark:bg-neutral-950/40 dark:text-neutral-300 dark:hover:bg-neutral-950/60 dark:focus-visible:outline-white" aria-label="next slide" x-on:click="next()">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-width="3" class="size-5 md:size-6 pl-0.5" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                    </svg>
+                </button>
+
+                {{-- slides --}}
+                {{-- Change min-h-[50svh] to your preferred height size --}}
+                <div class="relative min-h-[50svh] w-full">
+                    <template x-for="(slide, index) in slides">
+                        <div x-cloak x-show="currentSlideIndex == index + 1" class="absolute inset-0" x-transition.opacity.duration.1000ms>
+                            <div class="lg:px-32 lg:py-14 absolute inset-0 z-10 flex flex-col items-center justify-end gap-2 bg-gradient-to-t from-neutral-950/85 to-transparent px-16 py-12 text-center">
+                                <h3 class="w-full lg:w-[80%] text-balance text-2xl lg:text-3xl font-bold text-white" x-text="slide.title" x-bind:aria-describedby="'slide' + (index + 1) + 'Description'"></h3>
+                                <p class="lg:w-1/2 w-full text-pretty text-sm text-neutral-300" x-text="slide.description" x-bind:id="'slide' + (index + 1) + 'Description'"></p>
+                                <button type="button" x-cloak x-show="slide.ctaUrl !== null" class="mt-2 cursor-pointer whitespace-nowrap rounded-md border border-white bg-transparent px-4 py-2 text-center text-xs font-medium tracking-wide text-white transition hover:opacity-75 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-onSurfaceDarkStrong active:opacity-100 active:outline-offset-0 md:text-sm" x-text="slide.ctaText"></button>
+                            </div>
+
+                            <img class="absolute w-full h-full inset-0 object-cover text-neutral-600 dark:text-neutral-300" x-bind:src="slide.imgSrc" x-bind:alt="slide.imgAlt" />
+                        </div>
+                    </template>
+                </div>
+
+                {{-- indicators --}}
+                <div class="absolute rounded-md bottom-3 md:bottom-5 left-1/2 z-20 flex -translate-x-1/2 gap-4 md:gap-3 px-1.5 py-1 md:px-2" role="group" aria-label="slides" >
+                    <template x-for="(slide, index) in slides">
+                        <button class="size-2 cursor-pointer rounded-full transition" x-on:click="currentSlideIndex = index + 1" x-bind:class="[currentSlideIndex === index + 1 ? 'bg-neutral-300' : 'bg-neutral-300/50']" x-bind:aria-label="'slide ' + (index + 1)"></button>
+                    </template>
+                </div>
+            </div>
 
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet beatae consectetur consequuntur cupiditate ex
             harum
