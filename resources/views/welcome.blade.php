1d18e85861bbc967dc0098a3faeffc6fca97c158
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2025-01-10 07:22:37 +0100

    filter selectors refactored

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -289,17 +289,17 @@
             </div>
 
             {{-- Filter --}}
-            <div x-data="{ firstValue: '', secondValue: '' }" class="flex flex-col items-center px-2 gap-3 md:flex-row md:justify-center md:gap-3 md:items-center">
+            <div x-data="{ firstValue: '', secondValue: '' }" class="flex flex-col items-center px-2 gap-2 md:flex-row md:justify-center md:gap-0.5 md:items-center">
 
                 {{-- Icon --}}
                 <img class="size-10 sm:size-12 md:size-24 md:rotate-45" src="{{ asset('storage/objects/piper.svg') }}" alt="">
 
                 {{-- Double select --}}
-                <div class="flex flex-col w-full max-w-xs items-center gap-1 md:w-1/4 md:px-4 md:py-2">
+                <div class="flex flex-col w-full max-w-xs items-center gap-1 md:w-1/4 md:px-2 md:py-2">
                     {{-- Dish --}}
                     <div class="relative w-full">
                         <select id="modelName" name="modelName"
-                                class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-xs focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black disabled:cursor-not-allowed disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white md:px-4 md:py-2 md:text-sm"
+                                class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-xs focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-75 md:px-4 md:py-2 md:text-sm"
                                 x-model="firstValue">
                             <option value="" selected>Any dish</option>
                             <option value="camery">Broth</option>
@@ -312,10 +312,10 @@
                 </div>
 
                 {{-- Dish type --}}
-                <div class="flex flex-col w-full max-w-xs items-center gap-2 md:w-1/4 md:px-4 md:py-2">
+                <div class="flex flex-col w-full max-w-xs items-center gap-2 md:w-1/4 md:px-2 md:py-2">
                     <div class="relative w-full">
                         <select id="dish-type" name="dish-type"
-                                class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-xs focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black disabled:cursor-not-allowed disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white md:px-4 md:py-2 md:text-sm"
+                                class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-xs disabled:cursor-not-allowed disabled:opacity-75 md:px-4 md:py-2 md:text-sm"
                                 x-model="secondValue"
                                 :disabled="!firstValue">
                             <option value="" :selected="!firstValue" x-text="firstValue ? 'Any type' : 'Any type'"></option>
@@ -328,8 +328,8 @@
                 </div>
 
                 {{-- Select cuisine --}}
-                <div class="w-full max-w-xs md:w-1/4 md:px-4 md:py-2">
-                    <select id="country" name="country" class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-xs focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black disabled:cursor-not-allowed disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white md:px-4 md:py-2 md:text-sm">
+                <div class="w-full max-w-xs md:w-1/4  md:py-2">
+                    <select id="country" name="country" class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-xs disabled:cursor-not-allowed disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white md:px-4 md:py-2 md:text-sm">
                         <option value="Any" selected>Any cuisine</option>
                         <option value="Australia">Australian</option>
                         <option value="Belgium">Belgian</option>
@@ -349,8 +349,8 @@
                 </div>
 
                 {{-- Select menu --}}
-                <div class="w-full max-w-xs md:w-1/4 md:px-4 md:py-2">
-                    <select id="country" name="country" class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-xs focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black disabled:cursor-not-allowed disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white md:px-4 md:py-2 md:text-sm">
+                <div class="w-full max-w-xs md:w-1/4 md:px-1 md:py-2">
+                    <select id="country" name="country" class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-xs disabled:cursor-not-allowed disabled:opacity-75 dark:border-neutral-700 dark:bg-neutral-900/50 dark:focus-visible:outline-white md:px-4 md:py-2 md:text-sm">
                         <option value="Any" selected>Any menu</option>
                         <option value="Ketogenic">Ketogenic</option>
                         <option value="Gluten-free">Gluten-free</option>
