3e45c365834015cef118a1ebdd5a1a6f1726e1cb
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2025-01-29 09:01:03 +0100

    added relationships between User & UserProfiles models

--- app/Models/UserProfiles.php
+++ app/Models/UserProfiles.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserProfiles extends Model
 {
-    //
+    protected $fillable = [
+        'gender',
+        'birth_date',
+        'description',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
