19adf9f5c5f85fd0a33c5dda480fcba712f71fff
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2025-02-14 10:52:38 +0100

    some code refactored
    added new path to user photo on user-profile view & switched <form> to <a> on user-profile view

--- resources/views/user/user-profile.blade.php
+++ resources/views/user/user-profile.blade.php
@@ -9,7 +9,7 @@
                         <!-- Users info -->
                         <div class="flex flex-col items-center relative">
                             <!-- Edit profile button -->
-                            <form action="#edit-profile" class="absolute end-0">
+                            <a href="{{ route('profiles.edit', $user) }}" class="absolute end-0">
                                 @csrf
                                 <button class="rounded-lg p-1 bg-gourmania hover:gourmania-hover transition-colors duration-200">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="white" class="size-5">
@@ -17,10 +17,10 @@
                                         <path d="M4.75 3.5c-.69 0-1.25.56-1.25 1.25v6.5c0 .69.56 1.25 1.25 1.25h6.5c.69 0 1.25-.56 1.25-1.25V9A.75.75 0 0 1 14 9v2.25A2.75 2.75 0 0 1 11.25 14h-6.5A2.75 2.75 0 0 1 2 11.25v-6.5A2.75 2.75 0 0 1 4.75 2H7a.75.75 0 0 1 0 1.5H4.75Z" />
                                     </svg>
                                 </button>
-                            </form>
+                            </a>
                             <div>
                                 <!-- image -->
-                                <img src="{{ asset($user->photo) }}" class="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0" alt="User photo">
+                                <img src="{{ asset('./storage/' . $user->photo) }}" class="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0" alt="User photo">
                             </div>
                             <!-- name -->
                             <h1 class="text-xl font-bold">{{ $user->name }}</h1>
