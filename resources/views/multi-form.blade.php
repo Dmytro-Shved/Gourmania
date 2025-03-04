5c2f50f65feb40fdb1efef1441be13037407f549
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2025-03-04 10:40:02 +0100

    created 2 step in the wizzard form

--- resources/views/multi-form.blade.php
+++ resources/views/multi-form.blade.php
@@ -146,6 +146,8 @@
                     </select>
                 </div>
 
+                <br>
+
                 <!-- Next step button -->
                 <div class="flex justify-end">
                     <button type="button"
@@ -157,22 +159,38 @@
         </div>
     </div>
 
+    ========================================
+    ========================================
+    ========================================
+
+    @php
+        $units = [
+            'kg(s)', 'g(s)', 'piece(s)',
+            'head(s)', 'liter(s)', 'to taste',
+            'bunche(s)', 'twig(s)', 'stem(s)'
+            ]
+    @endphp
+
     {{--STEP 2--}}
     <div class="bg-gray-100 flex items-center justify-center p-4 font-inclusive">
         <div class="max-w-md w-full bg-white rounded-xl shadow-lg p-8">
             <!-- Stepper -->
             <ol class="flex w-full items-center justify-between gap-2 sm:gap-4 relative" aria-label="registration progress">
-                <!-- step 1 (current) -->
+                <!-- step 1 (completed) -->
                 <li class="flex items-center sm:gap-2 text-sm relative w-full justify-center sm:justify-start" aria-label="recipe">
-                    <span class="text-white bg-gourmania flex items-center justify-center size-6 rounded-full">1</span>
+                    <span class="text-white bg-gourmania flex items-center justify-center size-6 rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                          <path fill-rule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd" />
+                        </svg>
+                    </span>
                     <span class="absolute top-8 text-xs sm:static sm:top-0 sm:text-base">Recipe</span>
                 </li>
 
                 <span class="h-0.5 w-full bg-gourmania" aria-hidden="true"></span>
 
-                <!-- step 2 -->
+                <!-- step 2  (current) -->
                 <li class="flex items-center sm:gap-2 text-sm relative w-full justify-center sm:justify-start" aria-current="step" aria-label="ingredients">
-                    <span class="flex size-6 items-center justify-center rounded-full border border-[#AE763E] text-on-primary">2</span>
+                    <span class="text-white bg-gourmania flex items-center justify-center size-6 rounded-full">2</span>
                     <span class="absolute top-8 text-xs sm:static sm:top-0 sm:text-base">Ingredients</span>
                 </li>
 
@@ -189,30 +207,182 @@
             <br>
 
             <div class="space-y-4">
-                <div>
-                    <label class="block text-sm font-medium text-gray-700 mb-1">Recipe name</label>
-                    <input
-                        name="email"
-                        type="text"
-                        class="w-full px-4 py-2 border border-gray-300 rounded-lg gourmania-focus"
-                        placeholder="Chicken Broth"
-                        value="{{ old('email') }}"
-                    />
-                </div>
+                <table class="w-full text-left table-auto">
+                    <thead>
+                    <tr>
+                        <th class="text-sm font-medium text-gray-700 pt-2 text-center">Ingredient</th>
+                        <th class="text-sm font-medium text-gray-700 pt-2 text-center">Quantity</th>
+                        <th class="text-sm font-medium text-gray-700 pt-2 text-center">Unit</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <!-- Ingredient Name -->
+                        <td class="py-2 pr-1 w-1/3">
+                            <input
+                                type="text"
+                                class="w-full px-2 py-2 border border-gray-300 rounded-lg gourmania-focus text-sm"
+                                placeholder="Chicken"
+                            />
+                        </td>
 
-                <div>
-                    <label class="block text-sm font-medium text-gray-700 mb-1">Recipe name</label>
-                    <input
-                        name="email"
-                        type="text"
-                        class="w-full px-4 py-2 border border-gray-300 rounded-lg gourmania-focus"
-                        placeholder="your@email.com"
-                        value="{{ old('email') }}"
-                    />
+                        <!-- Quantity -->
+                        <td class="py-2 w-1/3">
+                            <div class="flex justify-center">
+                                <input
+                                    placeholder="1"
+                                    type="number"
+                                    class="w-full px-3 py-1.5 border border-gray-300 rounded-lg gourmania-focus"
+                                    min="1"
+                                />
+                            </div>
+                        </td>
+
+                        <!-- Unit -->
+                        <td class="py-2 pl-1 w-1/3">
+                            <select
+                                name="menu"
+                                class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 text-sm gourmania-focus disabled:cursor-not-allowed disabled:opacity-75 px-2 py-2"
+                                autocomplete="off"
+                            >
+                                @foreach($units as $unit)
+                                    <option value="{{ $unit }}">{{ $unit }}</option>
+                                @endforeach
+                            </select>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+
+                <br>
+
+                <div class="flex justify-between">
+                    <!-- Previous step button -->
+                    <div class="flex justify-start">
+                        <button type="button"
+                                class="w-[100px] bg-gray-500 hover:bg-gray-400 text-white font-medium py-2.5 rounded-lg transition-colors">
+                            Previous
+                        </button>
+                    </div>
+
+                    <!-- Next step button -->
+                    <div class="flex justify-end">
+                        <button type="button"
+                                class="w-[100px] bg-gourmania hover:gourmania-hover text-white font-medium py-2.5 rounded-lg transition-colors">
+                            Next
+                        </button>
+                    </div>
                 </div>
-                <button type="submit" class="w-full bg-gourmania hover:gourmania-hover text-white font-medium py-2.5 rounded-lg transition-colors">
-                    Continue
-                </button>
+            </div>
+        </div>
+    </div>
+
+    {{--STEP 3--}}
+    <div class="bg-gray-100 flex items-center justify-center p-4 font-inclusive">
+        <div class="max-w-md w-full bg-white rounded-xl shadow-lg p-8">
+            <!-- Stepper -->
+            <ol class="flex w-full items-center justify-between gap-2 sm:gap-4 relative" aria-label="registration progress">
+                <!-- step 1 (completed) -->
+                <li class="flex items-center sm:gap-2 text-sm relative w-full justify-center sm:justify-start" aria-label="recipe">
+                    <span class="text-white bg-gourmania flex items-center justify-center size-6 rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                          <path fill-rule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd" />
+                        </svg>
+                    </span>
+                    <span class="absolute top-8 text-xs sm:static sm:top-0 sm:text-base">Recipe</span>
+                </li>
+
+                <span class="h-0.5 w-full bg-gourmania" aria-hidden="true"></span>
+
+                <!-- step 2 (completed) -->
+                <li class="flex items-center sm:gap-2 text-sm relative w-full justify-center sm:justify-start" aria-current="step" aria-label="ingredients">
+                    <span class="text-white bg-gourmania flex items-center justify-center size-6 rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                          <path fill-rule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd" />
+                        </svg>
+                    </span>
+                    <span class="absolute top-8 text-xs sm:static sm:top-0 sm:text-base">Ingredients</span>
+                </li>
+
+                <span class="h-0.5 w-full bg-gourmania" aria-hidden="true"></span>
+
+                <!-- step 3 (current) -->
+                <li class="flex items-center sm:gap-2 text-sm relative w-full justify-center sm:justify-start" aria-label="guide">
+                    <span class="text-white bg-gourmania flex items-center justify-center size-6 rounded-full">3</span>
+                    <span class="absolute top-8 text-xs sm:static sm:top-0 sm:text-base">Guide</span>
+                </li>
+            </ol>
+
+            <br>
+            <br>
+
+            <div class="space-y-4">
+                <table class="w-full text-left table-auto">
+                    <thead>
+                    <tr>
+                        <th class="text-sm font-medium text-gray-700 pt-2 text-center">Ingredient</th>
+                        <th class="text-sm font-medium text-gray-700 pt-2 text-center">Quantity</th>
+                        <th class="text-sm font-medium text-gray-700 pt-2 text-center">Unit</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <!-- Ingredient Name -->
+                        <td class="py-2 pr-1 w-1/3">
+                            <input
+                                type="text"
+                                class="w-full px-2 py-2 border border-gray-300 rounded-lg gourmania-focus text-sm"
+                                placeholder="Chicken"
+                            />
+                        </td>
+
+                        <!-- Quantity -->
+                        <td class="py-2 w-1/3">
+                            <div class="flex justify-center">
+                                <input
+                                    placeholder="1"
+                                    type="number"
+                                    class="w-full px-3 py-1.5 border border-gray-300 rounded-lg gourmania-focus"
+                                    min="1"
+                                />
+                            </div>
+                        </td>
+
+                        <!-- Unit -->
+                        <td class="py-2 pl-1 w-1/3">
+                            <select
+                                name="menu"
+                                class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 text-sm gourmania-focus disabled:cursor-not-allowed disabled:opacity-75 px-2 py-2"
+                                autocomplete="off"
+                            >
+                                @foreach($units as $unit)
+                                    <option value="{{ $unit }}">{{ $unit }}</option>
+                                @endforeach
+                            </select>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+
+                <br>
+
+                <div class="flex justify-between">
+                    <!-- Previous step button -->
+                    <div class="flex justify-start">
+                        <button type="button"
+                                class="w-[100px] bg-gray-500 hover:bg-gray-400 text-white font-medium py-2.5 rounded-lg transition-colors">
+                            Previous
+                        </button>
+                    </div>
+
+                    <!-- Next step button -->
+                    <div class="flex justify-end">
+                        <button type="button"
+                                class="w-[100px] bg-gourmania hover:gourmania-hover text-white font-medium py-2.5 rounded-lg transition-colors">
+                            Next
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
