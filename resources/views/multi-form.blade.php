ac4c2ff952c50be14ed772afc6b60c43e5943bb5
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2025-03-04 11:52:53 +0100

    added new button & removed spaces between steps

--- resources/views/multi-form.blade.php
+++ resources/views/multi-form.blade.php
@@ -159,10 +159,6 @@
         </div>
     </div>
 
-    ========================================
-    ========================================
-    ========================================
-
     @php
         $units = [
             'kg(s)', 'g(s)', 'piece(s)',
@@ -253,6 +249,14 @@
                     </tr>
                     </tbody>
                 </table>
+                <!-- Add new block -->
+                <div class="flex justify-end mt-3">
+                    <button type="button" class="w-[35px] bg-gourmania hover:gourmania-hover text-white font-medium py-2.5 rounded-lg transition-colors flex justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4 ">
+                            <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+                        </svg>
+                    </button>
+                </div>
 
                 <br>
 
@@ -353,6 +357,7 @@
                 </div>
             </div>
 
+            <!-- Add new block -->
             <div class="flex justify-end mt-3">
                 <button type="button" class="w-[35px] bg-gourmania hover:gourmania-hover text-white font-medium py-2.5 rounded-lg transition-colors flex justify-center">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4 ">
