02ffc1d5d290df45a1e9c70c3db55135b753d117
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2025-05-15 18:14:00 +0200

    added caching into Livewire/Filter for dish categories, cuisines, menus

--- app/Livewire/Filter.php
+++ app/Livewire/Filter.php
@@ -24,20 +24,31 @@ class Filter extends Component
 
     public function mount()
     {
+        // Get id of parameter from request()
         $this->dishCategory = request()->get('dish_category', null);
         $this->dishSubcategory = request()->get('dish_subcategory', null);
-
         $this->cuisine = request()->get('cuisine', null);
         $this->menu = request()->get('menu', null);
 
-        $this->dishCategories = DishCategory::all();
+        // Cache Dish Categories for 24h
+        $this->dishCategories = cache()->remember('dish-categories', 60*60*24, function (){
+            return DishCategory::get();
+        });
 
+        // Get Dish Category by given Dish Subcategory id
         $this->dishSubcategories = $this->dishCategory
             ? DishSubcategory::where('dish_category_id', $this->dishCategory)->get()
             : collect();
 
-        $this->cuisines = Cuisine::get();
-        $this->menus = Menu::get();
+        // Cache cuisines for 24h
+        $this->cuisines = cache()->remember('cuisines', 60*60*24, function (){
+            return Cuisine::get();
+        });
+
+        // Cache menus for 24h
+        $this->menus = cache()->remember('menus', 60*60*24, function (){
+            return Menu::get();
+        });
     }
 
     public function render()
