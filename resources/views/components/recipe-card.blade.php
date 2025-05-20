abaa73dd36e6d5c4e2f5ad17303dde80612a707e
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2025-05-20 21:46:08 +0200

    refactored cook_time in recipe-card

--- resources/views/components/recipe-card.blade.php
+++ resources/views/components/recipe-card.blade.php
@@ -46,13 +46,13 @@
             </p>
 
             <!-- Cook time -->
-            <p class="flex items-center gap-1" title="{{ sprintf('%02d hour(s) | %02d minute(s)', ...explode(':', $recipe->cook_time)) }}">
+            <p class="flex items-center gap-1">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke-width="1"
                      stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round"
                           d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                 </svg>
-                {{ $recipe->cook_time }}
+                {{ sprintf('%02dh %02dmin', ...explode(':', $recipe->cook_time)) }}
             </p>
         </div>
 
