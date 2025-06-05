91dcda0dd403c05c2fd58993360904b0ec4e5662
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2025-06-05 15:29:37 +0200

    changed some index.blade.php styles, changed filter.blade.php styles

--- resources/views/livewire/filter.blade.php
+++ resources/views/livewire/filter.blade.php
@@ -4,7 +4,7 @@
     <div class="title-container select-none">
         <span class="flex-grow border-b border-black"></span>
         <small
-            class="font-inclusive text-xs xs:text-[14px] sm:text-[16px] md:text-[18px] lg:text-xl xl:text-2xl text-black px-4">
+            class="font-inclusive text-lg xl:text-2xl text-black px-4">
             Filter recipes
         </small>
         <span class="flex-grow border-t border-black"></span>
@@ -12,7 +12,7 @@
 
     <div class="flex flex-col items-center px-2 gap-2 md:flex-row md:justify-center md:gap-0.5 md:items-center font-inclusive">
         <!--Icon -->
-        <img class="size-10 sm:size-12 md:size-24 md:rotate-45 select-none" src="{{ asset('storage/objects/piper.svg') }}" alt="Pepper" rel="preload">
+        <img class="size-12 sm:size-14 md:size-24 md:rotate-45 select-none" src="{{ asset('storage/objects/piper.svg') }}" alt="Pepper" rel="preload">
 
         <!-- Double select -->
         <div class="flex flex-col w-full max-w-xs items-center gap-1 md:w-1/4 md:px-2 md:py-2">
@@ -20,7 +20,7 @@
             <div class="relative w-full">
                 <select wire:model.live="dishCategory"
                         name="dish_category"
-                        class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-xs gourmania-focus disabled:cursor-not-allowed disabled:opacity-75 md:px-4 md:py-2 md:text-sm"
+                        class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-base gourmania-focus disabled:cursor-not-allowed disabled:opacity-75 md:px-4 md:py-2 md:text-sm"
                         autocomplete="off"
                 >
                     <option value="">Any category</option>
@@ -36,7 +36,7 @@
             <div class="relative w-full">
                 <select wire:model.live="dishSubcategory"
                         name="dish_subcategory"
-                        class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 gourmania-focus px-2 py-1 text-xs disabled:cursor-not-allowed disabled:opacity-75 md:px-4 md:py-2 md:text-sm"
+                        class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-base gourmania-focus disabled:cursor-not-allowed disabled:opacity-75 md:px-4 md:py-2 md:text-sm"
                         autocomplete="off">
 
                     <option value="" selected>Any dish</option>
@@ -52,7 +52,7 @@
         <div class="w-full max-w-xs md:w-1/4  md:py-2">
             <select wire:model.live="cuisine"
                 name="cuisine"
-                class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-xs disabled:cursor-not-allowed disabled:opacity-75 gourmania-focus md:px-4 md:py-2 md:text-sm"
+                class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-base disabled:cursor-not-allowed disabled:opacity-75 gourmania-focus md:px-4 md:py-2 md:text-sm"
                 autocomplete="off"
             >
 
@@ -68,7 +68,7 @@
         <div class="w-full max-w-xs md:w-1/4 md:px-1 md:py-2">
             <select wire:model.live="menu"
                 name="menu"
-                class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-xs disabled:cursor-not-allowed disabled:opacity-75 gourmania-focus md:px-4 md:py-2 md:text-sm"
+                class="w-full appearance-none rounded-md border border-neutral-300 bg-neutral-50 px-2 py-1 text-base disabled:cursor-not-allowed disabled:opacity-75 gourmania-focus md:px-4 md:py-2 md:text-sm"
                 autocomplete="off"
             >
 
@@ -82,7 +82,7 @@
         <!-- Filter button -->
         <div class="w-full max-w-xs md:w-auto md:px-4 md:py-2 md:text-base">
             <button type="submit"
-                    class="font-inclusive text-neutral-200 text-sm bg-gourmania hover:gourmania-hover transition rounded-xl px-4 py-1 w-full md:px-4 md:py-2 md:text-base">
+                    class="font-inclusive text-neutral-200 text-lg bg-gourmania hover:gourmania-hover transition rounded-xl px-4 py-1 w-full md:px-4 md:py-2 md:text-base">
                 Filter
             </button>
         </div>
