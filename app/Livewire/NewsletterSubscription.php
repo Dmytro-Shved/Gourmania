3675349575711e0e055cb174530e8742dd4cd64c
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2025-07-02 09:46:49 +0200

    added livewire component NewsletterSubscription and fixed the code

---
+++ app/Livewire/NewsletterSubscription.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use MailchimpMarketing\ApiClient;
+
+class NewsletterSubscription extends Component
+{
+    public string $api_key = '';
+    public string $server_prefix = '';
+    public string $list_id = '';
+    public string $subscriber_hash = '';
+    public string $email_address = '';
+
+
+    public function mount()
+    {
+        $this->api_key = config('services.mailchimp.api_key');
+        $this->server_prefix = config('services.mailchimp.server_prefix');
+        $this->list_id = config('services.mailchimp.list_id');
+    }
+
+    public function subscribe()
+    {
+        // Validate email
+        $this->validate([
+            'email_address' => ['required', 'email']
+        ]);
+
+        // Initialize client
+        // basic setConfig() code
+        $client = $this->create_client();
+
+        // Set subscriber hash
+        $subscriber_hash = md5(strtolower($this->email_address));
+
+        // Catch response to show message
+        try {
+            $response = $client->lists->setListMember(
+                $this->list_id,
+                $subscriber_hash, [
+                    "email_address" => $this->email_address,
+                    "status_if_new" => "pending"
+                ]);
+
+            if ($response->status === "pending") {
+                // for pending
+                session()->flash('success', 'Please check your email to confirm subscription.');
+            } elseif ($response->status === "subscribed") {
+
+                // for subscribed
+                session()->flash('info', "You're already subscribed! Your profile was updated");
+            }
+
+            $this->reset('email_address');
+
+        } catch (\Exception $e) {
+            $errorMessage = $e->getMessage();
+
+            if (str_contains($errorMessage, "already exists")) {
+                $this->addError('email_address', "You're already subscribed");
+            } else {
+                $this->addError('email_address', 'Error: ' . $errorMessage);
+            }
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.newsletter-subscription');
+    }
+
+    public function create_client()
+    {
+        $client = new ApiClient();
+
+        $client->setConfig([
+            'apiKey' => $this->api_key,
+            'server' => $this->server_prefix,
+        ]);
+
+        return $client;
+    }
+}
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -70,32 +70,7 @@
         <div class="container px-6 py-12 mx-auto">
             <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
 
-                {{-- Email subscribtion --}}
-                <div class="sm:col-span-2">
-                    <h1 class="max-w-lg text-[10] text-center font-inclusive tracking-tight text-white px-1 xl:text-md italic py-1">
-                        Subscribe our newsletter to get a new recipes and dishes of the week!
-                    </h1>
-
-                    <form action="{{ route('newsletter.subscribe') }}" method="POST" class="flex flex-col mt-6 space-y-3 md:space-y-0 md:flex-row px-3 my-3 bg-[#DDB892] rounded p-3">
-                        @csrf
-                        <input id="sub-email"
-                               name="email"
-                               type="email"
-                               class="px-4 py-2 text-gray-700 font-serif border border-white bg-white rounded-lg focus:outline-none focus:ring-none focus:border-transparent focus:ring-2 focus:ring-[#AE763E]"
-                               placeholder="E-mail address"/>
-
-                        <button
-                            type="submit"
-                            class="w-full px-6 py-2.5 text-sm font-inclusive tracking-wider text-white transition-colors duration-300 transform md:w-auto md:mx-4 focus:outline-none bg-[#592D00] rounded-lg hover:bg-[#C58F5C]">
-                            Subscribe
-                        </button>
-                    </form>
-
-                    {{-- Subscribed message --}}
-                    @if(session('subscribed'))
-                        <p class="mt-2 text-sm text-green-600 font-semibold">{{ session('subscribed') }}</p>
-                    @endif
-                </div>
+                <livewire:newsletter-subscription />
 
                 {{-- Quick links--}}
                 <div>
