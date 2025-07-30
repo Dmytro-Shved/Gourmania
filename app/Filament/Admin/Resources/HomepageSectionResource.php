49e11ad14654c1590ab8348e885760e652f79c3a
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2025-07-30 10:36:38 +0200

    added HomepageSectionResource, refactored icons

---
+++ app/Filament/Admin/Resources/HomepageSectionResource.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\HomepageSectionResource\Pages;
+use App\Filament\Admin\Resources\HomepageSectionResource\RelationManagers;
+use App\Models\HomepageSection;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class HomepageSectionResource extends Resource
+{
+    protected static ?string $model = HomepageSection::class;
+
+    protected static ?string $navigationIcon = 'https://i.ibb.co/G4bwQK95/homepage-section-inactive.png';
+    protected static ?string $activeNavigationIcon = 'https://i.ibb.co/W4smQ9c1/homepage-section-icon.png';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListHomepageSections::route('/'),
+            'create' => Pages\CreateHomepageSection::route('/create'),
+            'edit' => Pages\EditHomepageSection::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Admin/Resources/HomepageSectionResource/Pages/CreateHomepageSection.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\HomepageSectionResource\Pages;
+
+use App\Filament\Admin\Resources\HomepageSectionResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateHomepageSection extends CreateRecord
+{
+    protected static string $resource = HomepageSectionResource::class;
+}
