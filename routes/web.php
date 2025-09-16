7c8fe32fa9909266ef7b51a8b4e7d8c5ed4e0a88
Dmytro Shved <u_shved.dmytro@tdgjar.edu.pl>
2025-09-16 08:52:14 +0200

    refactor: prefix, name, groups for routes

--- routes/web.php
+++ routes/web.php
@@ -17,71 +17,71 @@ Route::get('/', HomeController::class)->name('home');
 
 // Guest routes
 Route::middleware('guest')->group(function (){
+    // Login
     Route::view('/login', 'auth.login')->name('login-page');
     Route::post('/login', LoginController::class)->name('login');
 
+    // Register
     Route::view('/register', 'auth.register')->name('register-page');
     Route::post('/register', RegisterController::class)->name('register');
 });
 
+// Logout
+Route::post('/logout', LogoutController::class)->middleware('auth')->name('logout');
+
 // Profile
-Route::get('/users/profiles/{user}', [ProfileController::class, 'showProfile'])->name('profiles.show');
-Route::get('/users/profiles/{user}/saved', [ProfileController::class, 'savedRecipes'])->name('profiles.saved');
-Route::get('/users/profiles/{user}/liked', [ProfileController::class, 'likedRecipes'])->name('profiles.liked');
-Route::get('/users/profiles/{user}/edit', [ProfileController::class, 'edit'])->name('profiles.edit');
-Route::put('/users/profiles/{user}', [ProfileController::class, 'update'])->name('profiles.update');
-Route::delete('/users/profiles/{user}', [ProfileController::class, 'destroy'])->name('profiles.destroy');
+Route::prefix('users/profiles')->name('profiles.')->group(function (){
+    Route::get('{user}', [ProfileController::class, 'showProfile'])->name('show');
+    Route::get('{user}/saved', [ProfileController::class, 'savedRecipes'])->name('saved');
+    Route::get('{user}/liked', [ProfileController::class, 'likedRecipes'])->name('liked');
+    Route::get('{user}/edit', [ProfileController::class, 'edit'])->name('edit');
+    Route::put('{user}', [ProfileController::class, 'update'])->name('update');
+    Route::delete('{user}', [ProfileController::class, 'destroy'])->name('destroy');
+});
 
-// Show Recipes
-Route::get('/recipes', [RecipeController::class, 'index'])->name('recipes.index');
-Route::get('/recipes/{recipe}/guide', [RecipeController::class, 'guide'])->name('recipes.guide');
-Route::get('/recipes/search', [RecipeController::class, 'search'])->name('recipes.search');
+// Recipes
+Route::prefix('recipes')->name('recipes.')->group(function (){
+    Route::get('', [RecipeController::class, 'index'])->name('index');
+    Route::get('{recipe}/guide', [RecipeController::class, 'guide'])->name('guide');
+    Route::get('search', [RecipeController::class, 'search'])->name('search');
 
-// Email Verification
-Route::middleware('auth')->group(function (){
-    Route::view('/email/verify', 'auth.verify-email')->name('verification.notice');
-    Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])
-        ->middleware('signed')
-        ->name('verification.verify');
-    Route::post('/email/verification-notification', [EmailVerificationController::class, 'resend'])
-        ->middleware('throttle:6,1')
-        ->name('verification.send');
+    Route::middleware(['auth', 'verified'])->group(function (){
+        Route::view('create','recipes.recipe-create')->name('create');
+        Route::get('{recipe}/edit', [RecipeController::class, 'edit'])->name('edit');
+        Route::delete('{recipe}', [RecipeController::class, 'destroy'])->name('destroy');
+    });
+});
+
+// Email Verification *auth
+Route::prefix('email')->name('verification.')->middleware('auth')->group(function (){
+    Route::view('verify', 'auth.verify-email')->name('notice');
+    Route::get('verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])->middleware('signed')->name('verify');
+    Route::post('verification-notification', [EmailVerificationController::class, 'resend'])->middleware('throttle:6,1')->name('send');
 });
 
 // Password Reset
-Route::middleware('guest')->group(function (){
-    Route::view('/forgot-password', 'auth.forgot-password')->name('password.request');
-    Route::post('/forgot-password', [PasswordResetController::class, 'email'])->name('password.email');
-    Route::get('/reset-password/{token}', [PasswordResetController::class, 'reset'])->name('password.reset');
-    Route::post('/reset-password', [PasswordResetController::class, 'update'])->name('password.update');
-});
-
-// Auth routes
-Route::middleware('auth')->group(function (){
-    // Logout
-    Route::post('/logout', LogoutController::class)->name('logout');
-
-    // Verified Routes
-    Route::middleware('verified')->group(function (){
-        // Update Recipe
-        Route::view('/recipes/create','recipes.recipe-create')->name('recipes.create');
-        Route::get('/recipes/{recipe}/edit', [RecipeController::class, 'edit'])->name('recipes.edit');
-        Route::delete('/recipes/{recipe}', [RecipeController::class, 'destroy'])->name('recipes.destroy');
-    });
+Route::name('password.')->middleware('guest')->group(function (){
+    Route::view('/forgot-password', 'auth.forgot-password')->name('request');
+    Route::post('/forgot-password', [PasswordResetController::class, 'email'])->name('email');
+    Route::get('/reset-password/{token}', [PasswordResetController::class, 'reset'])->name('reset');
+    Route::post('/reset-password', [PasswordResetController::class, 'update'])->name('update');
 });
 
 // FAQ Page
 Route::view('/help/faq', 'help.faq')->name('faq');
 
 // Basics routes
-Route::view('/basics', 'basics.index')->name('basics');
-Route::get('basics/tools', [BasicsController::class, 'tools'])->name('tools');
-Route::get('basics/techniques', [BasicsController::class, 'techniques'])->name('techniques');
+Route::prefix('basics')->group(function (){
+    Route::view('', 'basics.index')->name('basics');
+    Route::get('tools', [BasicsController::class, 'tools'])->name('tools');
+    Route::get('techniques', [BasicsController::class, 'techniques'])->name('techniques');
+});
 
 // Legal routes
 Route::get('/privacy', [LegalController::class, 'privacy'])->name('privacy');
 Route::get('/terms', [LegalController::class, 'terms'])->name('terms');
 
+// Fallback
 Route::fallback(function (){
      abort(404);
 });
